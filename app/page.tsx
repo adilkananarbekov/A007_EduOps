3856b36aea97e29054893a15114d57b0164c7326
AlbarsbekovAzim <albarsbekovazim@gmail.com>
2026-04-19 21:51:09 +0600

    reset commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,65 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-
-  const router = useRouter();
-  const [dots, setDots] = useState(1);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDots((prev) => (prev === 3 ? 1 : prev + 1));
-    }, 150); // speed control here
-
-    // const role = localStorage.getItem("role");
-    // const token = localStorage.getItem("token");
-    const userDataRaw = localStorage.getItem("userData");
-    // if (!token || !role) {
-    //   router.replace("/login");
-    // }
-    if (!userDataRaw) {
-      router.replace("/login");
-      return;
-    }
-    const userData = JSON.parse(userDataRaw);
-    const token = userData.token;
-    const role = userData.role;
-    if (role === "ADMIN") {
-      (async () => {
-        const res = await fetch("http://136.116.64.6/api/admin/users", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          }
-        });
-        if (res.ok) {
-          router.replace("/admin");
-        } else {
-          router.replace("/login");
-        }
-      })();
-      
-    } else if (role === "STUDENT") {
-      router.replace("/parent");
-    } else {
-      router.replace("/login");
-    }
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1>Loading{".".repeat(dots)}</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
